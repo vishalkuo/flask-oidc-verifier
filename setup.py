d0e11de00ba66afa26798085e9aa3a2298f49a01
Vishal Kuo <vishal.kuo@robinhood.com>
2020-03-29 10:57:44 -0700

    bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flask_oidc_verifier",
-    version="0.0.2",
+    version="0.0.3",
     author="Vishal Kuo",
     author_email="vishalkuo@gmail.com",
     description="Run oidc token verification",
